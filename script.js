f04db1093c41758c2a8ee984793565acaca29b0a
Zaigham Salehi <zsalehi313@gmail.com>
2023-12-22 15:25:37 -0500

    linked stylesheet and script.js, initialized gameboard state

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// Gameboard state 
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+};
+
+// create the Gameboard array
+for (let i = 0; i < rows, i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+        board[i].push(Cell());
+    }
+}
+
+// User constructor
+function User() {
+    let name;
+    
+}
